4080a9eba7c77eda51467c8f0a7b0ea035555ff4
gaby28894178 <28894178+gaby28894178@users.noreply.github.com>
2026-01-15 20:46:20 -0300

    tarea 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# clase1_con_leandro" 
+la  funcion para imprimir  en pantalla  se llama  print(" hola mundo ")   la misma se   coloca  entre  parentesis  y comillas  dobles simple  o triple 
---
+++ clase_1.py
@@ -0,0 +1,54 @@
+# # imprimo  mi  saludo  hola  musndo por consola
+
+# texto =  ''' 
+# SOMOS  UN  STRING  DE  MUCHAS  
+# LINEAS  DE  CODIFO 
+
+# '''
+# texto2 = " hola mundo "
+ 
+# texto3 = 'soy leandro'
+
+# # print ("hola  mundo desde  python ")
+# print(texto2 + texto3 )
+
+
+# supo  dos  numeros  enteros  
+
+# num1 = "12"
+# num2 = 1
+
+# print (num1 +num2)
+
+
+
+# creo  una  resta  de  dos  numeros  
+
+# num1 = 22
+# num2 = 20
+# resta = num2 - num1
+# # print("La resta  es: ",resta)
+# print(f"La resta  es: {resta}")  # template string  o cadenas  f 
+
+
+# ingrese  un  entero  de  la edad 18 de  uin  usuario y verifique si puede  ingresar al  boliche 
+#estructura  condicional  simple 
+# edad = int(input("Ingrese  su edad: "))
+
+# if edad > 18:
+#     print(f"Puede ingresar es  mayor su edad es de: {edad}")
+
+
+# ingrese  un  entero  de  la edad 18 de  uin  usuario y verifique si puede  ingresar al  boliche  si no  no puede  ingresar 
+# edad = int(input("Ingrese  su edad: "))
+
+# if edad >= 18:
+#      print(f"Puede ingresar es  mayor su edad es de: {edad}")
+# else:
+#      print(f"NO Puede ingresar No es  mayor su edad es de: {edad}")
+    
+
+
+
+
+
